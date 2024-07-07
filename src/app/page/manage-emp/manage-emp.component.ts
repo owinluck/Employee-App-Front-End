b259ee725d442c3c5a8d581fae258b3d799b8a4c
owinluck <owinlakshitha@gmail.com>
2024-07-08 01:35:02 +0530

    Sweet alart

--- src/app/page/manage-emp/manage-emp.component.ts
+++ src/app/page/manage-emp/manage-emp.component.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { first } from 'rxjs';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-manage-emp',
@@ -28,7 +29,12 @@ export class ManageEmpComponent {
     
     this.http.post("http://localhost:8080/emp-controller/add-employee",this.employeeOb).subscribe(
       (data) =>{
-        console.log(data);
+        
+        Swal.fire({
+          title: "Employee Added!",
+          text: "You clicked the button!",
+          icon: "success"
+        });
         
       }
     )
